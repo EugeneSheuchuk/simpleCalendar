3b4e324045376b80d22167203de437f099e10092
Yauheni Sheuchuk <shevchuked@tut.by>
2020-03-17 10:42:24 +0300

    print current year

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const currentDate = new Date();
+  let currentYear = currentDate.getFullYear();
   return (
     <div>
-
+      {currentYear}
     </div>
   );
 }
